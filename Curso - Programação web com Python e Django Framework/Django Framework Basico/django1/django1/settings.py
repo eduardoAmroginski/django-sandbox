504d825256528dd8fe286708c01d0523fa94f7fc
eduardoAsantos <eduardo-amroginski@hotmail.com>
2021-08-31 15:06:51 -0300

    Fazendo primeiros ajustes em settings.py e criando primeiras views em views.py

--- Curso - Programação web com Python e Django Framework/Django Framework Basico/django1/django1/settings.py
+++ Curso - Programação web com Python e Django Framework/Django Framework Basico/django1/django1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fz#y^bgi%nf4on^w=8#yl!7&ov9!cd-z01xznaaj3avo32mx!f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
