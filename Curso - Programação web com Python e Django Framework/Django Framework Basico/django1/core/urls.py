aa1ab01a630614e388d028d00715520895ce3562
Eduardo Amroginski Santos <eduardo-amroginski@ibm.com>
2021-09-13 17:49:09 -0300

    Interando produtos do banco de dados e buscando por produtos especificos

--- Curso - Programação web com Python e Django Framework/Django Framework Basico/django1/core/urls.py
+++ Curso - Programação web com Python e Django Framework/Django Framework Basico/django1/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, contato
+from .views import index, contato, produto
 
 urlpatterns = [
-    path('', index),
-    path('contato', contato)
+    path('', index, name='index'),
+    path('contato', contato, name='contato'),
+    path('produto/<int:id>', produto, name='produto')
 ]
--- Curso - Programação web com Python e Django Framework/Django Framework Basico/django1/core/views.py
+++ Curso - Programação web com Python e Django Framework/Django Framework Basico/django1/core/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render
-
+from core.models import Produto
 # Create your views here.
 
 
 def index(request):
-    print(dir(request.user))
-    print(f"User: {request.user}")
+    # print(dir(request.user))
+    # print(f"User: {request.user}")
 
     if str(request.user) == 'AnonymousUser':
         teste = 'Usuário não logado'
     else:
         teste = 'Usuário logado'
     
+    produtos = Produto.objects.all()
+
     context = {
         'curso': 'Programação web com Django Framework',
         'outro': 'Django é massa!',
-        'logado': teste
+        'logado': teste,
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, id):
+    item = Produto.objects.get(id=id)
+    context = {
+        'produto': item
+    }
+    return render(request, 'produto.html', context)
