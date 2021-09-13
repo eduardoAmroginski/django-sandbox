4a2e964fc0b09d18e9f382d9cf88c98a221782cf
Eduardo Amroginski Santos <eduardo-amroginski@ibm.com>
2021-09-13 16:30:50 -0300

    Adicionando conteudo ao banco de dados pelo Django Shell

--- Curso - Programação web com Python e Django Framework/Django Framework Basico/django1/core/views.py
+++ Curso - Programação web com Python e Django Framework/Django Framework Basico/django1/core/views.py
@@ -4,9 +4,18 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(dir(request.user))
+    print(f"User: {request.user}")
+
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuário não logado'
+    else:
+        teste = 'Usuário logado'
+    
     context = {
         'curso': 'Programação web com Django Framework',
-        'outro': 'Django é massa!'
+        'outro': 'Django é massa!',
+        'logado': teste
     }
     return render(request, 'index.html', context)
 
